5c9ee0960d255149b44f2df5fc6f8622e6bbda30
Amit-rajput97 <84862054+Amit-rajput97@users.noreply.github.com>
2022-01-16 13:19:30 +0530

    Create checkmax.java
    check the maximum in two number.

---
+++ checkmax.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class max
+{
+    int num1;
+    int num2;
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Numbers:");
+        num1=sc.nextInt();
+        num2=sc.nextInt();
+    }
+    public void m1()
+    {
+        if (num1>num2) {
+            System.out.println(num1+" is greater than "+num2);
+        }
+        else {
+            System.out.println(num2+" is greater than "+num1);
+        }
+//        int maxnum = Math.max(num1,num2);
+//        System.out.println(maxnum);
+    }
+}
+public class checkmax {
+    public static void main(String[] args) {
+        max obj =new max();
+        obj.m1();
+    }
+}
